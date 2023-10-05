dfaa8e5b03cbb52790a9e024274d8dcc7c41ac24
R.Grimaudo <riccardo.grimaudo2002@gmail.com>
2023-10-05 09:07:30 +0200

    prova commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// c
 export class AppComponent {
   title = 'people';
 }
